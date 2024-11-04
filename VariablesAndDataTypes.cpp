24608ff87ab13407d90fb3cc8b34d903b019a58b
ram <rramkumar1306@gmail.com>
2024-11-04 14:41:25 +0530

    Refactore swap_two_numbers method.
    Added feature for to mutate the original value.

--- VariablesAndDataTypes.cpp
+++ VariablesAndDataTypes.cpp
@@ -1,13 +1,32 @@
 #include "VariablesAndDataTypes.h"
 #include <iostream>
-
+#include <cstdio>
 
 int main() {
-	VariablesAndDataTypes::swap_two_numbers(10, 20);
+	int a = 10;
+	int b = 20;
+	VariablesAndDataTypes::swap_two_numbers(a, b, false);
+	std::cout << "A" << a << "B" << b << "\n";
+	VariablesAndDataTypes::swap_two_numbers(a, b);
+	std::cout << "A" << a << "B" << b << "\n";
 	return 0;
 }
 
-void VariablesAndDataTypes::swap_two_numbers(int a, int b)
+void VariablesAndDataTypes::swap_two_numbers(int &a, int &b, bool mutateOriginal)
+{
+	if (mutateOriginal) 
+	{
+		swap_helper(a, b);
+	}
+	else
+	{
+		int tempA = a;
+		int tempB = b;
+		swap_helper(tempA, tempB);
+	}
+}
+
+void VariablesAndDataTypes::swap_helper(int& a, int& b)
 {
 	std::cout << "Before Swap a: " << a << " and b: " << b << "\n";
 	int temp;
@@ -15,5 +34,4 @@ void VariablesAndDataTypes::swap_two_numbers(int a, int b)
 	a = b;
 	b = temp;
 	std::cout << "After Swap a: " << a << " and b: " << b << "\n";
-
 }
--- VariablesAndDataTypes.h
+++ VariablesAndDataTypes.h
@@ -3,6 +3,10 @@ class VariablesAndDataTypes
 {
 public:
 // swap two numbers
-	static void swap_two_numbers(int a, int b);
+	static void swap_two_numbers(int &a, int &b, bool mutateOriginal = true);
+
+private:
+	static void swap_helper(int& a, int& b);
+	     
 };
 

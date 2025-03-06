937b88e5fd194ae7c392e9235a8840f4ea69e5da
ram <rramkumar1306@gmail.com>
2025-03-06 14:16:45 +0530

    feat: display student records

--- StudentRecordSystem/main.cpp
+++ StudentRecordSystem/main.cpp
@@ -28,7 +28,31 @@ void addStudent()
     std::cout << "Student added successfully!\n";
 };
 
+void displayStudents()
+{
+    std::cout << "\n--- Student Records ---\n";
+    for(const auto student : students)
+    {
+        std::cout << "ID: " << student.id << ", Name: " << student.name << ", Age: " << student.age << ", Grade: " << student.grade << "\n";
+    };
+};
+
 int main()
 {
+    int choice;
+
+    do
+    {
+        std::cout << "\n1. Add Student\n2. Display Students\n3. Exit\nChoose: ";
+        std::cin >> choice;
+        switch (choice) {
+            case 1: addStudent(); break;
+            case 2: displayStudents(); break;
+            case 3: std::cout << "Exiting... \n"; break;
+            default: std::cout << "Invalid choice! Try again.\n";
+        
+        }
+    }while(choice != 3);
+
     return 0;
 }

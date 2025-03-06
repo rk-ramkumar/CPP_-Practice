d2368d198fa745e21c09f135522243d0e0180bb2
ram <rramkumar1306@gmail.com>
2025-03-06 13:59:27 +0530

    feat: add student details

--- StudentRecordSystem/main.cpp
+++ StudentRecordSystem/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 struct Student {
     int id;
     std::string name;
     int age;
     float grade;
+};
+
+std::vector<Student> students;
+
+void addStudent()
+{
+    Student s;
+    std::cout << "Enter ID: ";
+    std::cin >> s.id;
+    std::cout << "Enter Name: ";
+    std::cin.ignore();
+    std::getline(std::cin, s.name);
+    std::cout << "Enter Age: ";
+    std::cin >> s.age;
+    std:: cout << "Enter grade: ";
+    std::cin >> s.grade;
+
+    students.push_back(s);
+    std::cout << "Student added successfully!\n";
 };
 
 int main()

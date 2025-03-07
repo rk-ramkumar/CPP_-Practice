2e3d7bab9cfeaba87dae9411265624472f619d32
ram <rramkumar1306@gmail.com>
2025-03-07 19:39:19 +0530

    feat(bms): derive current and savings form account

--- BankManagementSystem/main.cpp
+++ BankManagementSystem/main.cpp
@@ -3,16 +3,32 @@
 
 class Account{
     public:
-        std::string holderName;
-        int id;
+        Account(std::string name, int initialPayment = 0, int minBal = 0): holderName(name), balance(initialPayment), minimumBalance(minBal) {}
         
         void withdraw(int amount);
         void deposit(int amount);
         void checkBalance();
         
     protected:
+        std::string holderName;
+        int id;
         int balance;
+        int minimumBalance;
+    
 };
+class CurrentAccount: public Account 
+{
+    public:
+        CurrentAccount(std::string name, int initialPayment = 0): Account(name, initialPayment, 1000) {} ;
+};
+
+class SavingAccount: public Account 
+{
+    protected:
+        int interestRate;
+        int transactionLimit;
+};
+
 
 int main()
 {
